e1debc151e7f51ba105c042ca145a10cfec5ffc5
Hariharen <57080243+hariharen9@users.noreply.github.com>
2020-08-09 13:52:26 +0530

    assets, google fonts added  default comments removed

--- lib/editors_picks.dart
+++ lib/editors_picks.dart
@@ -1,4 +1,6 @@
-class editors  extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+class EditorsPick extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();

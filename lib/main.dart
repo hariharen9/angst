84417c89565ba83e622e8246e4990403b749cf09
BalaganeshSomu <65117631+BalaganeshSomu@users.noreply.github.com>
2020-08-09 13:04:17 +0530

    EDITORS pick CHANGE : added a class

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Angst',
       theme: ThemeData(
         // This is the theme of your application.
-        //
+        // hiuhiu
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke

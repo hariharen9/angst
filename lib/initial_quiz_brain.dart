d73f2feb31e75bf6cd2d13e41b17d249d790d1ec
BalaganeshSomu <65117631+BalaganeshSomu@users.noreply.github.com>
2020-08-09 16:49:31 +0530

    Added : 1. A constructor for questions and answers [questions.dart] 2. A class containing the questions and answers for initial quiz [inital_quiz_brain.dart] 3. A class containing the questions for answers final quiz [should_i_see_a_doctor_quiz_brain.dart]

---
+++ lib/initial_quiz_brain.dart
@@ -0,0 +1,26 @@
+import 'questions.dart';
+
+class QuizBrain_initial {
+  List<Question> questionBank = [
+    Question(
+        q:'Do you feel quite restless/irritable lately?',
+        a:true),
+    Question(
+        q:'Are you experiencing reduced concentration lately?',
+        a:true),
+    Question(
+        q:'Are you experiencing a lot of racing thoughts lately?',
+        a:true),
+    Question(
+        q:'Are you experiencing one or more of the following feelings? Excessive worrying, Mild numbness , Sudden fear ',
+        a:true),
+    Question(
+        q:'Do you experience a sense of choking and shortness of breath , like someone\'s holding your chest tightly suddenly?',
+        a:true),
+
+  ];
+}
+
+// SCORES
+// 0-2    YOU MIGHT HAVE MILD TO MODERATE ANXIETY
+// 3-5    YOU MIGHT HAVE MODERATE TO HIGH ANXIETY
---
+++ lib/should_i_see_a_doctor_quiz_brain.dart
@@ -0,0 +1,26 @@
+import 'questions.dart';
+
+class QuizBrain_final {
+  List<Question> questionBank = [
+    Question(
+        q:'You have absolutely no control over you worries',
+        a:true),
+    Question(
+        q:'You feel like your thoughts are affecting your day to day well being',
+        a:true),
+    Question(
+        q:'Are you experiencing suicidal or self harming thoughts lately?',
+        a:true),
+    Question(
+        q:'You are experiencing the symptoms for more than 6 months',
+        a:true),
+    Question(
+        q:'Anxiety is affecting more than one area of your life',
+        a:true),
+
+  ];
+}
+
+// SCORES
+// 1 or more :
+// ask to consult a doctor

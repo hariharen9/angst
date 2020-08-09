be430ae75d9c604eceee53ebbffa7a3aba997edb
Hariharen <57080243+hariharen9@users.noreply.github.com>
2020-08-09 15:38:32 +0530

    Two txt added in home, TODO: sized box realignment is required

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,16 +15,49 @@ class Homepage extends StatelessWidget {
             ),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              SizedBox(
+                height: 200,
+              ),
+              SizedBox(
+                height: 200,
+              ),
               Expanded(
-                child: Text('Trouvaille Inc.',
-                     style: GoogleFonts.audiowide(
-                       textStyle: TextStyle(
-                         fontSize: 50,
-                       )
-                     ),
+                child: Center(
+                  child: Text(
+                    'TROUVAILLE INC.',
+                    style: GoogleFonts.robotoCondensed(
+                        textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                    )),
+                  ),
                 ),
-              )
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'A N G S T',
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                height: 100,
+              ),
             ],
           ) /* add child content here */,
         ),

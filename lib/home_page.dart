cce3d83fdfc4ef65471c729da644a1e85c39ba28
Hariharen <57080243+hariharen9@users.noreply.github.com>
2020-08-09 15:17:59 +0530

    image added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,7 +5,29 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(),
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Text('Trouvaille Inc.',
+                     style: GoogleFonts.audiowide(
+                       textStyle: TextStyle(
+                         fontSize: 50,
+                       )
+                     ),
+                ),
+              )
+            ],
+          ) /* add child content here */,
+        ),
+      ),
     );
   }
 }

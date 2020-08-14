58693977cd740e0c9dde8ea37f1db8ac8d0b9ec3
BalaganeshSomu <65117631+BalaganeshSomu@users.noreply.github.com>
2020-08-14 16:10:34 +0530

    - Exercises are added in main.dart file as a list

--- lib/exercises.dart
+++ lib/exercises.dart
@@ -9,7 +9,25 @@ class _ExercisesState extends State<Exercises> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('fuck off'),
+      child: Text('hey'),
     );
   }
 }
+
+
+//  EXERCISES
+List exercises =[
+  //1
+  'If you are feeling anxious right now, Stop what you are doing and concentrate on your breath.', //this screen should have the timer
+  //2
+  'If you are feeling like you are out of your body , Touch five things around you and constantly remind yourself the You got this!',
+  //3
+  'Write down your thoughts and answer these questions'
+  'Why are you feeling anxious right now?'
+  'What is your thought pattern right now? Let your mind wander and try to understand wht it\'s trying to say'
+  'What triggers your anxiety? It can be anything like driving or speaking to people. Identify it and try to find a remedy',
+  //4
+  'Keep reassuring yourself that things would be alright and chant a mantra of your own! ',
+  //5
+  'Sometimes people can feel less anxious when they have physical contact with people they love. Ask your loved ones for a big bear hug!'
+];
